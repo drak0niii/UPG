85cec716283206a936ae1585f2881de19338c2eb
drak0niii <121613553+drak0niii@users.noreply.github.com>
2022-12-29 13:46:07 +0200

    Add files via upload

---
+++ UPG Live/UPG Live/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UPG Live
+//
+//  Created by Costin Ionita on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func OpenTab(_ sender: Any) {
+        
+
+    print("button pressed ")
+        
+    }
+    
+
+}
+
